13c36eee3b0aed80b23b0941ceda12eae73313a4
Bluscream <bluscream@users.noreply.github.com>
2026-07-26 13:07:42 +0200

    Configure dev.bluscream.common_vrc with dev.bluscream.common dependency and clean duplicate code

---
+++ Packages/dev.bluscream.common_vrc/Editor/VRCCommonHelper.cs
@@ -0,0 +1,13 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace Bluscream.VRC
+{
+    /// <summary>
+    /// Common VRChat utilities and helper methods
+    /// </summary>
+    public static class VRCCommonHelper
+    {
+        public const string VRC_COMMON_VERSION = "1.0.0";
+    }
+}
