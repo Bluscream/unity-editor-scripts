813a09be1a8266cb6e2242be6c660380ae31f501
Bluscream <bluscream@users.noreply.github.com>
2026-07-27 22:58:26 +0200

    fix(avataroptimizer_vrc): add using static Bluscream.Utils to AvatarContactOptimizer and AvatarConstraintOptimizer

--- Packages/dev.bluscream.avataroptimizer_vrc/Editor/AvatarContactOptimizer.cs
+++ Packages/dev.bluscream.avataroptimizer_vrc/Editor/AvatarContactOptimizer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
 using UnityEngine;
+using static Bluscream.Utils;
 using static Bluscream.TransformExtensions;
 
 namespace Bluscream.VRCAvatarOptimizer
